6a872928692469a0e14849744676afc72a3bf965
oshij <oshijtrivedi@gmail.com>
2023-03-14 12:21:47 +0530

    users

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import App from './App';
-import { store } from './Redux/Store';
 
 ReactDOM.render(
-  <Provider store={store} >
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
